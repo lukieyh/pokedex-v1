661d0fbf65cfd40c71b0f6f0863bf50812723f8c
unknown <lukiey.git@gmail.com>
2022-07-26 20:01:57 -0300

    Pokedex v1

---
+++ Pokedex-v1/js/script.js
@@ -0,0 +1,63 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
+const form = document.querySelector(".form");
+const inputSearch = document.querySelector(".input_search");
+const buttonPrev = document.querySelector(".btn-prev");
+const buttonNext = document.querySelector(".btn-next");
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (APIResponse.status === 200) {
+  const data = await APIResponse.json();
+  return data;
+
+}
+}
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = "loading...";
+  pokemonNumber.innerHTML = "";
+  
+  const data = await fetchPokemon(pokemon);
+  
+  if (data) {
+  pokemonImage.style.display = "block";
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+
+  inputSearch.value = "";
+  searchPokemon = data.id
+} else {
+  pokemonImage.style.display = "none";
+  pokemonName.innerHTML = "Not found :(";
+  pokemonNumber.innerHTML = "";
+
+}
+}
+
+form.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+  renderPokemon(inputSearch.value.toLowerCase());
+ 
+});
+
+buttonPrev.addEventListener("click", () => {  
+  if (searchPokemon > 1) {
+  searchPokemon -= 1;
+  renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener("click", () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon); 
+});
+
+renderPokemon(searchPokemon);
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pokedex-v1
-Pokedex with API
+# Pokédex
+
+### *Portuguese version*
+
+construído com HTML, CSS, javaScript.
+
+usando a API [https://pokeapi.co/](https://pokeapi.co/), para chamar os dados para pokedex.
+
+![149](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/e9ae5d88-c3e8-4c04-8ede-f85b7cc019c8/Untitled.png)
+
+---
+
+# Pokédex
+
+### E*nglish version*
+
+built with HTML, CSS, javaScript.
+using API [https://pokeapi.co/](https://pokeapi.co/), to call the data for pokedex.
+
+![448](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/62d16e2c-5184-439b-a5e2-56ac27b5c3f1/Untitled.png)
+
+---
